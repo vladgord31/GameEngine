5ed6dcc1c2b1f8d32c0f7d9238357e09543864cb
Weyn <gordijcukvlad64@gmail.com>
2025-04-04 10:48:02 +0300

    ImGui settings

--- Core/src/Core/Window.cpp
+++ Core/src/Core/Window.cpp
@@ -4,6 +4,9 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
 namespace Engine
 {
     static bool s_GLFW_initialized = false;
@@ -12,6 +15,10 @@ namespace Engine
 		: m_data({std::move(title), width, height})
 	{
 		int returnCode = init();
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui_ImplOpenGL3_Init();
 	}
 
 	Window::~Window()
@@ -99,6 +106,18 @@ namespace Engine
 
         glClearColor(1, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGuiIO& io = ImGui::GetIO();
+        io.DisplaySize.x = static_cast<float>(get_width());
+        io.DisplaySize.y = static_cast<float>(get_height());
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(m_pWindow);
         glfwPollEvents();
 	}
